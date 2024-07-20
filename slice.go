c8484b9e5b3cffaff1e5f9034bd29255c5cb14d1
Fabio Pereira <fabiuhp@msn.com>
2024-07-20 16:20:15 -0300

    declaracoes de array e slice

---
+++ slice.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//maneiras de definir um slice
+	var slice []string
+	slice2 := []int{}
+	slice3 := []int{0, 1, 2, 3}
+
+	//maneiras de definir array
+	array := make([]int, 1, 1)
+	array2 := [4]int{0, 1, 2, 3}
+}
