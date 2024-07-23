29f8be3c82dd57b72e54ec8320dae0adc3dbe4f8
Fabio Pereira <fabiuhp@msn.com>
2024-07-23 09:16:49 -0300

    inicio do livro | busca binaria

---
+++ grokking-algorithms/binary-search.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	result, err := BinarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8}, 13)
+	if err != nil {
+		fmt.Printf("Erro: %v\n", err)
+	} else {
+		fmt.Printf("Valor final: %v\n", result)
+	}
+}
+
+func BinarySearch(array []int, chave int) (int, error) {
+	menor := 0
+	maior := len(array) - 1
+
+	for menor <= maior {
+		meio := (menor + maior) / 2
+		posicao := array[meio]
+
+		if posicao == chave {
+			return posicao, nil
+		} else if posicao > chave {
+			maior = meio - 1
+		} else {
+			menor = meio + 1
+		}
+	}
+	return 0, errors.New("Não encontrado.")
+}
