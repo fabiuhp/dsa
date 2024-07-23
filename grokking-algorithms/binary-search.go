fd3ff9addb3ac8e096133935f47e55ee7e47ba4a
Fabio Pereira <fabiuhp@msn.com>
2024-07-23 09:19:57 -0300

    inicio do livro | busca binaria

--- grokking-algorithms/binary-search.go
+++ grokking-algorithms/binary-search.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	result, err := BinarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8}, 13)
+	result, err := BinarySearch([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 13)
 	if err != nil {
 		fmt.Printf("Erro: %v\n", err)
 	} else {
